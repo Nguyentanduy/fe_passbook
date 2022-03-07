b1e08ed75e455e31bac1a6c65f83af35f25a3f91
duy hoang <duyhoang3820@gmail.com>
2022-03-07 22:46:20 +0700

    update signIn signUp

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import cx from "classnames";
 import styles from "./style.module.scss";
+// import { Redirect } from 'react-router'
 
 export default function Login() {
   return (
     <div className={cx(styles.wrapper, styles.fadeInDown)}>
       <div id="formContent">
         {/* Tabs Titles */}
-        <h2 className={styles.active}> Sign In </h2>
-        <h2 className={cx(styles.inactive, styles.underlineHover)}>Sign Up </h2>
-        {/* <h2 className="inactive underlineHover">Sign Up </h2> */}
+        <h2 className={styles.active}> Đăng nhập </h2>
+        <h2 onClick={() =>{
+          // <Redirect to="/register"/>
+        }} className={cx(styles.inactive, styles.underlineHover)}>Đăng ký</h2>
         {/* Icon */}
         <div className={cx(styles.fadeIn, styles.first)}>
-          {/* <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> */}
         </div>
         {/* Login Form */}
         <form>
           <input
             type="text"
-            id="login"
+            id="email"
             className={cx(styles.fadeIn, styles.second)}
-            name="login"
-            placeholder="username"
+            name="email"
+            placeholder="EMAIL"
           />
           <input
             type="text"
             id="password"
             className={cx(styles.fadeIn, styles.third)}
-            name="login"
-            placeholder="password"
+            name="password"
+            placeholder="MẬT KHẨU"
           />
           <input type="submit" className={cx(styles.fadeIn, styles.fourth)} />
         </form>
         {/* Remind Passowrd */}
         <div id="formFooter" className={cx(styles.formFooter)}>
           <a className={styles.underlineHover} href="#">
-            Forgot Password?
+            Quên mật khẩu?
           </a>
         </div>
       </div>
--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,36 +1,75 @@
-import React from 'react';
-import styles from './style.module.scss'
+import React from "react";
+import styles from "./style.module.scss";
 import cx from "classnames";
+import "antd/dist/antd.css";
+import { DatePicker } from "antd";
+// import { Redirect } from 'react-router'
 
 export default function Register() {
-  return <div className={cx(styles.wrapper, styles.fadeInDown)}>
-  <div id="formContent">
-    {/* Tabs Titles */}
-    <h2 className={styles.active}> Sign Up </h2>
-    <h2 className={cx(styles.inactive, styles.underlineHover)}>Sign In </h2>
-    {/* <h2 className="inactive underlineHover">Sign Up </h2> */}
-    {/* Icon */}
-    <div className={cx(styles.fadeIn, styles.first)}>
-      {/* <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" /> */}
+  function onChange(date, dateString) {
+    console.log(date, dateString);
+  }
+  return (
+    <div className={cx(styles.wrapper, styles.fadeInDown)}>
+      <div id="formContent">
+        {/* Tabs Titles */}
+        <h2 className={styles.active}> Đăng ký </h2>
+        <h2 onClick={()=>{
+          // <Redirect to="/login"/>
+        }} className={cx(styles.inactive, styles.underlineHover)}> Đăng nhập </h2>
+        <div className={cx(styles.fadeIn, styles.first)}>
+        </div>
+        {/* Login Form */}
+        <form>
+          <input
+            type="email"
+            id="email"
+            className={cx(styles.fadeIn, styles.second)}
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            id="password"
+            className={cx(styles.fadeIn, styles.third)}
+            name="password"
+            placeholder="Mật khẩu"
+          />    
+          <input
+            type="text"
+            id="lastName"
+            className={cx(styles.fadeIn, styles.fiveth)}
+            name="lastName"
+            placeholder="HỌ"
+          />
+          <input
+            type="text"
+            id="firstName"
+            className={cx(styles.fadeIn, styles.fourth)}
+            name="firstName"
+            placeholder="TÊN"
+          />
+          <input
+            type="text"
+            id="address"
+            className={cx(styles.fadeIn, styles.sixth)}
+            name="address"
+            placeholder="ĐỊA CHỈ"
+          />
+          <input
+            type="number"
+            id="idCard"
+            className={cx(styles.fadeIn, styles.seventh)}
+            name="idCard"
+            placeholder="CMND/CCCD"
+          />
+          <div className={cx(styles.fadeIn, styles.dateOfBirth,styles.eighth)}>
+            <div>NGÀY SINH</div>
+            <DatePicker onChange={onChange} />
+          </div>
+          <input type="submit" className={cx(styles.fadeIn, styles.eighth)} />
+        </form>
+      </div>
     </div>
-    {/* Login Form */}
-    <form>
-      <input
-        type="text"
-        id="login"
-        className={cx(styles.fadeIn, styles.second)}
-        name="login"
-        placeholder="username"
-      />
-      <input
-        type="text"
-        id="password"
-        className={cx(styles.fadeIn, styles.third)}
-        name="login"
-        placeholder="password"
-      />
-      <input type="submit" className={cx(styles.fadeIn, styles.fourth)} />
-    </form>
-  </div>
-</div>;
+  );
 }
